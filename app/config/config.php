8d626cbf17c62a8f49ba64b6ae1fe146ccb3719c
Nilton Cesar <ncpma.ing@gmail.com>
2023-02-21 00:22:32 -0500

    add styles to main.css

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php 
 	define('DB_HOST', '127.0.0.1');
 	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	// define('DB_PASS', 'root');
+	// define('DB_PASS', '');
+	define('DB_PASS', 'root');
 	define('DB_NAME', 'scrum');
 
 	define('APPROOT', dirname(dirname(__FILE__)));
-	define('URLROOT', 'http://192.168.8.100/scrum-panel');
-	// define('URLROOT', 'http://144.22.165.160/scrum-panel');
+	// define('URLROOT', 'http://192.168.8.100/scrum-panel');
+	define('URLROOT', 'http://144.22.165.160/scrum-panel');
 	define('APPNAME', 'Panel Scrum Sys.');
 ?>
